8560e690a9007b9e15d264525d93659c23a62553
ortiza5 <43008712+ortiza5@users.noreply.github.com>
2022-08-14 17:37:58 -0600

    Remove redundant functions

--- js/background-helper.js
+++ js/background-helper.js
@@ -1,80 +1,15 @@
-const FOCUS_INDICATOR = {
-  ENABLED: true,
-  COLOR: "#50e3c2",
-  WIDTH: "2px",
-};
-
-function getStyleContent(options) {
-  return `
-    @keyframes outline-pulse {
-      from {
-        outline-color:  ${options.color};
-        outline-offset: -${options.width};
-      }
-
-      90% {
-        outline-color: transparent;
-        outline-offset: -1px;
-      }
-
-      to {
-        outline-color: ${options.color};
-        outline-offset: 0;
-      }
+chrome.storage.local.get(
+  {
+    enabled: FOCUS_INDICATOR.ENABLED,
+    color: FOCUS_INDICATOR.COLOR,
+    width: FOCUS_INDICATOR.WIDTH,
+  },
+  (options) => {
+    if (options.enabled) {
+      removeStyle();
+      addStyle(options);
+    } else {
+      removeStyle();
     }
-
-    :focus {
-      outline: ${options.width} solid ${options.color} !important;
-      outline-offset: 0 !important;
-      animation: outline-pulse 4s;
-    }
-  `;
-}
-
-function getStyleNode(options) {
-  const styleNode = document.createElement("style");
-  styleNode.setAttribute("data-ref", "focus-indicator");
-  styleNode.innerHTML = getStyleContent(options);
-  return styleNode;
-}
-
-function removeStyle() {
-  const head = document.head;
-  const styleNodes = document.querySelectorAll('[data-ref="focus-indicator"]');
-
-  styleNodes.forEach((styleNode) => {
-    if (styleNode) {
-      head.removeChild(styleNode);
-    }
-  });
-}
-
-function addStyle(options) {
-  const head = document.head;
-  const styleNode = getStyleNode(options);
-
-  head.appendChild(styleNode);
-}
-
-function controlStyle() {
-  chrome.storage.local.get(
-    {
-      enabled: FOCUS_INDICATOR.ENABLED,
-      color: FOCUS_INDICATOR.COLOR,
-      width: FOCUS_INDICATOR.WIDTH,
-    },
-    (options) => {
-      if (options.enabled) {
-        removeStyle();
-        addStyle(options);
-      } else {
-        removeStyle();
-      }
-    }
-  );
-}
-
-controlStyle();
-
-const event = new Event("updateShow");
-document.dispatchEvent(event);
+  }
+);
